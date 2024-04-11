5a432bbaff7a56daf56ddca50b938f63b166ae52
Mykyta Zhuravlov <88032641+ZhuravlovM@users.noreply.github.com>
2024-04-11 23:28:34 +0300

    lesson23 homework - completed: (#4)
    - custom User model is used instead of a default django user model
    - users.User is adjusted as AUTH_USER in django settings

--- src/users/api.py
+++ src/users/api.py
@@ -1,1 +1,34 @@
-from django.shortcuts import render  # NOQA F401
+import json
+
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import User
+
+
+@csrf_exempt
+def create_user(request: HttpRequest) -> JsonResponse:
+    if request.method != "POST":
+        raise NotImplementedError("Only POST requests")
+
+    data: dict = json.loads(request.body)
+
+    user = User.objects.create_user(
+        email=data.get("email"),
+        password=data.get("password"),
+        first_name=data.get("first_name"),
+        last_name=data.get("last_name"),
+        role=data.get("role"),
+        activated=data.get("activated", True),
+    )
+
+    results = {
+        "id": user.id,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "role": user.role,
+        "activated": user.is_active,
+    }
+
+    return JsonResponse(results)
--- src/users/models.py
+++ src/users/models.py
@@ -1,9 +1,44 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
+from .managers import UserManager
 
 
-class User(models.Model):
-    email = models.CharField(max_length=30)
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    password = models.CharField(max_length=100)
+    last_login = models.DateTimeField(default=timezone.now)
+    activated = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
     role = models.CharField(max_length=20)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def __str__(self):
+        if self.first_name and self.last_name:
+            return self.get_full_name()
+        else:
+            return self.email
